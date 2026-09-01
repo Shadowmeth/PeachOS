b0f2b7fab703d9ba18c5bc8b9c8a99e4d942d6a0
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2026-09-01 21:27:21 +0500

    feat: Added cross binutils bin path to $PATH in build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@
 export PREFIX="/opt/gcc-16.1.0/"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
+export PREFIX="/opt/binutils-2.47/"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+
 make all
