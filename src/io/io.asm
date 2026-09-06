ee8aab4e21ffb9aa705e8c18c0257e888274f157
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2026-09-06 21:00:58 +0500

    feat: Implemented outb, outw, insb and insw functions

---
+++ src/io/io.asm
@@ -0,0 +1,52 @@
+global .asm
+
+global insb
+global insw
+global outb
+global outw
+
+insb:
+        push ebp
+        mov ebp, esp
+
+        xor eax, eax
+        ; Port in edx register
+        mov edx, [ebp + 8]
+        in al, dx
+
+        pop ebp
+        ret
+
+insw:
+        push ebp
+        mov ebp, esp
+        
+        xor eax, eax
+        ; Port in edx register
+        mov edx, [ebp + 8]
+        in ax, dx
+
+        pop ebp
+        ret
+
+outb:
+        push ebp
+        mov ebp, esp
+
+        mov eax, [ebp + 12]
+        mov edx, [ebp + 8]
+        out dx, al
+
+        pop ebp
+        ret
+
+outw:
+        push ebp
+        mov ebp, esp
+
+        mov eax, [ebp + 12]
+        mov edx, [ebp + 8]
+        out dx, ax
+
+        pop ebp
+        ret
